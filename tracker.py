a15d28f25e5201d4bd3f686a1d0f5baeb96fc8b7
Yaroslav <545585@mail.ru>
2020-05-26 17:37:29 +0500

    third commit

--- tracker.py
+++ tracker.py
@@ -19,13 +19,19 @@ with m as source:
     print("Скажи")
     audio = r.listen(source)
 
-with m as source:
-    r.adjust_for_ambient_noise(source)
 
-query = r.recognize_google(audio, language="ru-RU")
-print("Вы сказали: " + query.lower())
+# query = r.recognize_google(audio, language="ru-RU")
+# print("Вы сказали: " + query.lower())
 
-# stop_listening = r.listen_in_background(m)
+# with m as source:
+#     r.adjust_for_ambient_noise(source)
+
+# def callback(Recognizer, audio): 
+#         voice = Recognizer.recognize_google(audio, language = "ru-RU").lower()
+#         print("[log] Распознано: " + voice)
+    
+
+# stop_listening = r.listen_in_background(m, callback)
 # while True: time.sleep(0.1) #infinity loop
 
 
@@ -109,12 +115,8 @@ class ActionHandler:
         :param entities:
         :return:
         """
-        default = list()
-        default.append(random.choice(["Не понял Вас, повторите", "Что-что? Повторите еще раз"]))
-        engine.say(random.choice(["Не понял Вас, повторите", "Что-что? Повторите еще раз"]))
-        engine.runAndWait()
-        if len(default) > 0:
-            return ". ".join(default)
+
+        return random.choice(["Не понял Вас, повторите", "Что-что? Повторите еще раз"])
 
     def on_twin_greeting(self, intent, entities):
         """
@@ -123,14 +125,10 @@ class ActionHandler:
         :param entities:
         :return:
         """
-        greeting = list()
         phrases1 = ["Добрый день!","Здравствуйте!","Приветствую вас!"]
         phrases2 = ["Какой у вас вопрос?", "Меня зовут Тоша. Чем я могу помочь?",""]
-        greeting.append ("{p1} {p2}".format(p1 = random.choice(phrases1), p2 = random.choice(phrases2)))
-        engine.say("{p1} {p2}".format(p1 = random.choice(phrases1), p2 = random.choice(phrases2)))
-        engine.runAndWait()
-        if len(greeting) > 0:
-            return ". ".join(greeting)
+        return "{p1} {p2}".format(p1 = random.choice(phrases1), p2 = random.choice(phrases2))
+
 
 
     def on_thanks(self, intent, entities):
@@ -140,12 +138,9 @@ class ActionHandler:
         :param entities:
         :return:
         """
-        thanks = list()
-        thanks.append(random.choice(["Пожалуйста, рад помочь", "Всегда пожалуйста, рад помочь", "Пожалуйста"]))
-        engine.say(random.choice(["Пожалуйста, рад помочь", "Всегда пожалуйста, рад помочь", "Пожалуйста"]))
-        engine.runAndWait()
-        if len(thanks) > 0:
-            return ". ".join(thanks)
+
+        return random.choice(["Пожалуйста, рад помочь", "Всегда пожалуйста, рад помочь", "Пожалуйста"])
+
 
     def on_twin_goodbye(self, intent, entities):
         """
@@ -154,12 +149,7 @@ class ActionHandler:
         :param entities:
         :return:
         """
-        goodbye = list ()
-        goodbye.append(random.choice(["Всего доброго", "До свидания", "Всегда рад помочь"]))
-        engine.say(random.choice(["Всего доброго", "До свидания", "Всегда рад помочь"]))
-        engine.runAndWait()
-        if len(goodbye) > 0:
-            return ". ".join(goodbye)
+        return random.choice(["Всего доброго", "До свидания", "Всегда рад помочь"])
 
 
     def on_twin_repeat(self, intent, entities):
@@ -182,92 +172,48 @@ class ActionHandler:
         # обрабатываем каждую сущость, которая может ходить с данным интентом
         if self.get_any_entity(entities,'swimming_pool'):
             ways.append("идите прямо"), 
-            engine.say("идите прямо")
-            engine.runAndWait()
         if self.get_any_entity(entities,'music_room'):
             ways.append("Поднимитесь на второй этаж и идите налево, вторая дверь")
-            engine.say("Поднимитесь на второй этаж и идите налево, вторая дверь")
-            engine.runAndWait()
         if self.get_any_entity(entities,'teaching_room'):
             ways.append("Поднимитесь на второй этаж и идите налево до конца коридора, третья дверь после групп")
-            engine.say("Поднимитесь на второй этаж и идите налево до конца коридора, третья дверь после групп")
-            engine.runAndWait()
         if self.get_any_entity(entities,'laundry'):
             ways.append("идите налево по коридору, третья дверь")
-            engine.say("идите налево по коридору, третья дверь")
-            engine.runAndWait()
         if self.get_any_entity(entities,'medical_office'):
             ways.append("идите налево по коридору, четвертая дверь")
-            engine.say("идите налево по коридору, четвертая дверь")
-            engine.runAndWait()
         if self.get_any_entity(entities,'HR'):
             ways.append("Поднимитесь на второй этаж и идите налево до конца коридора, первая дверь после групп")
-            engine.say("Поднимитесь на второй этаж и идите налево до конца коридора, первая дверь после групп")
-            engine.runAndWait()
         if self.get_any_entity(entities,'accounting'):
             ways.append("Поднимитесь на второй этаж и идите налево до конца коридора, четвертая дверь после групп")
-            engine.say("Поднимитесь на второй этаж и идите налево до конца коридора, четвертая дверь после групп")
-            engine.runAndWait()
         if self.get_any_entity(entities,'food_block'):
             ways.append("идите налево до железной двери и поверните направо перед ней")
-            engine.say("идите налево до железной двери и поверните направо перед ней")
-            engine.runAndWait()
         if self.get_any_entity(entities,'manager'):
             ways.append("Поднимитесь на второй этаж и идите налево до конца коридора, вторая дверь после групп ")
-            engine.say("Поднимитесь на второй этаж и идите налево до конца коридора, вторая дверь после групп")
-            engine.runAndWait()
         if self.get_any_entity(entities,'gym'):
             ways.append("Поднимитесь на второй этаж и идите налево, первая дверь")
-            engine.say("Поднимитесь на второй этаж и идите налево, первая дверь")
-            engine.runAndWait()
         if self.get_any_entity(entities,'group_1'):
             ways.append("идите направо по коридору, первая дверь")
-            engine.say("идите направо по коридору, первая дверь")
-            engine.runAndWait()
         if self.get_any_entity(entities,'group_2'):
             ways.append("идите направо по коридору, вторая дверь")
-            engine.say("идите направо по коридору, вторая дверь")
-            engine.runAndWait()
         if self.get_any_entity(entities,'group_3'):
             ways.append("идите направо по коридору, третья дверь")
-            engine.say("идите направо по коридору, третья дверь")
-            engine.runAndWait()
         if self.get_any_entity(entities,'group_4'):
             ways.append("идите направо по коридору, четвертая дверь")
-            engine.say("идите направо по коридору, четвертая дверь")
-            engine.runAndWait()
         if self.get_any_entity(entities,'group_5'):
             ways.append("идите налево по коридору, первая дверь")
-            engine.say("идите налево по коридору, первая дверь")
-            engine.runAndWait()
         if self.get_any_entity(entities,'group_6'):
             ways.append("идите налево по коридору, вторая дверь")
-            engine.say("идите налево по коридору, вторая дверь")
-            engine.runAndWait()
         if self.get_any_entity(entities,'group_7'):
             ways.append("Поднимитесь на второй этаж и идите направо, первая дверь ")
-            engine.say("Поднимитесь на второй этаж и идите направо, первая дверь")
-            engine.runAndWait()
         if self.get_any_entity(entities,'group_8'):
             ways.append("Поднимитесь на второй этаж и идите направо, вторая дверь ")
-            engine.say("Поднимитесь на второй этаж и идите направо, вторая дверь")
-            engine.runAndWait()
         if self.get_any_entity(entities,'group_9'):
             ways.append("Поднимитесь на второй этаж и идите направо, третья дверь ")
-            engine.say("Поднимитесь на второй этаж и идите направо, третья дверь")
-            engine.runAndWait()
         if self.get_any_entity(entities,'group_10'):
             ways.append("Поднимитесь на второй этаж и идите направо, четвертая дверь ")
-            engine.say("Поднимитесь на второй этаж и идите направо, четвертая дверь")
-            engine.runAndWait()
         if self.get_any_entity(entities,'group_11'):
             ways.append("Поднимитесь на второй этаж и идите налево, первая дверь")
-            engine.say("Поднимитесь на второй этаж и идите налево, первая дверь")
-            engine.runAndWait()
         if self.get_any_entity(entities,'group_12'):
             ways.append("Поднимитесь на второй этаж и идите налево, вторая дверь")
-            engine.say("Поднимитесь на второй этаж и идите налево, вторая дверь")
-            engine.runAndWait()
             
        
         #  формируем ответ
@@ -290,28 +236,16 @@ class ActionHandler:
         # обрабатываем каждую сущость, которая может ходить с данным интентом
         if self.get_any_entity(entities,'accounting'):
             phone.append(str(sheet['B2'].value))
-            engine.say(str(sheet['B2'].value))
-            engine.runAndWait()
         if self.get_any_entity(entities,'manager'):
             phone.append(str(sheet['B3'].value))
-            engine.say(str(sheet['B3'].value))
-            engine.runAndWait()
         if self.get_any_entity(entities,'manager_chores'):
             phone.append(str(sheet['B4'].value))
-            engine.say(str(sheet['B4'].value))
-            engine.runAndWait()
         if self.get_any_entity(entities,'manager_education'):
             phone.append(str(sheet['B5'].value))
-            engine.say(str(sheet['B5'].value))
-            engine.runAndWait()
         if self.get_any_entity(entities,'medical_office'):
             phone.append(str(sheet['B6'].value))
-            engine.say(str(sheet['B6'].value))
-            engine.runAndWait()
         if self.get_any_entity(entities,'HR'):
             phone.append(str(sheet['B7'].value))
-            engine.say(str(sheet['B7'].value))
-            engine.runAndWait()
         
         #  формируем ответ
         if len(phone) > 0:
@@ -325,6 +259,9 @@ class ActionHandler:
         :param entities:
         :return:
         """
+
+        wb = openpyxl.load_workbook('tosha.xlsx')
+        sheet = wb['Меню']
         def prepare_menu(date: str, times: list):
             """
             Сходить в БД, получить меню
@@ -335,17 +272,17 @@ class ActionHandler:
             now_date = datetime.datetime.now().strftime('%Y-%m-%d')
             if date != now_date:
                 return "Меню на {date} нет".format(date=date)
-            return "Меню на {date}, время - {times}".format(date=date,times = ", ".join(times))
+            return "Меню {times}".format(date=date,times = ", ".join(times))
         # вытаскиваем дату или ставим текущую
         now_date = datetime.datetime.now().strftime('%Y-%m-%d')
         time_entity = self.get_any_entity(entities,'time',dict())
         date = time_entity.get('value',now_date).split('T')[0]
         # смотрим какое время хотят
         TIMES = (
-            ("time_breakfast","завтрак"),
-            ("time_dinner","обед"),
-            ("time_afternoon_tea","полдник"),
-            ("time_supper","ужин")
+            ("time_breakfast",sheet['B3'].value),
+            ("time_dinner",sheet['B4'].value),
+            ("time_afternoon_tea",sheet['B5'].value),
+            ("time_supper",sheet['B6'].value)
         )
         times = list()
         for e,v in TIMES:
@@ -355,6 +292,44 @@ class ActionHandler:
             times = [item[1] for item in TIMES]
         # возвращаем результат
         return prepare_menu(date,times)
+
+    def on_ask_TV(self,intent, entities):
+        """
+        :param intent:
+        :param entities:
+        :return:
+        """
+
+        wb = openpyxl.load_workbook('tosha.xlsx')
+        sheet = wb['Телепрограмма']
+        def prepare_tv(date: str, times: list):
+            """
+            Сходить в БД, получить телепрограмму
+            :param date:
+            :param times:
+            :return:
+            """
+        
+            fri_date = datetime.datetime(2020, 5, 29).strftime('%Y-%m-%d')
+            if date != fri_date:
+                return "Телепрограммы на {date} нет".format(date=date)
+            return "Телепрогамма {times}".format(date=date,times = ", ".join(times))
+        # вытаскиваем дату или ставим текущую
+        now_date = datetime.datetime.now().strftime('%Y-%m-%d')
+        time_entity = self.get_any_entity(entities,'time',dict())
+        date = time_entity.get('value',now_date).split('T')[0]
+        # смотрим какое время хотят
+        TIMES = (
+            ("TV",sheet['B2'].value),
+        )
+        times = list()
+        for e,v in TIMES:
+            if self.get_any_entity(entities,e):
+                times.append(v)
+        if len(times) < 1:
+            times = [item[1] for item in TIMES]
+        # возвращаем результат
+        return prepare_tv(date,times)
 
     # def on_ask_menu(self,intent, entities):
     #     """
@@ -413,10 +388,16 @@ if __name__ == "__main__":
     # сам диалог
     dialog = DialogTracker(interpreter = interpreter, action_handler=action_handler)
     while True:
-        print("Вы:")
+        with m as source:
+            print("Скажи")
+            audio = r.listen(source)
+        query = r.recognize_google(audio, language="ru-RU")
+        print("Вы сказали: " + query.lower())
         input_text = query
         output_text = dialog.predict(input_text)
         print("Бот:")
         print(output_text)
+        engine.say(output_text)
+        engine.runAndWait()
     pass
 
